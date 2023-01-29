4e12ba93e541b18355d060f11a92a1efdab18391
Furious-Alarm <Furious.Alarm@gmail.com>
2023-01-29 01:28:00 -0500

    Added some in-line documentation for clarification as to what some Go things do

--- src/main.go
+++ src/main.go
@@ -6,13 +6,13 @@ import (
 )
 
 // A single node in a data structure
-type node struct {
+type node struct {	// This struct is lowercased because it should only be used in the scope of this file
 	data interface{}
 	next *node
 }
 
 // A singlely linked list
-type LinkedList struct {
+type LinkedList struct {	// Go does not have classes, but it does have method functions for types
 	head *node
 	tail *node
 	Size int
@@ -21,7 +21,7 @@ type LinkedList struct {
 // Will add data to the end of the singlely linked list
 // Args:
 //		data (interface{}): the information to store in the list
-func (list *LinkedList) Append(data ...interface{}) {
+func (list *LinkedList) Append(data ...interface{}) {	// The ... functions like a "rest" parameter from Clojure. It is called a variadic parameter.
 	for i := range data {
 		item := &node{data[i], nil}
 
@@ -32,7 +32,7 @@ func (list *LinkedList) Append(data ...interface{}) {
 			list.tail.next = item
 		}
 
-		list.tail = item; list.Size++
+		list.tail = item; list.Size++	// You can execute multiple lines of code on one with a semicolon. This is not to be confused with initializer syntax for if statements
 	}
 }
 
@@ -55,7 +55,7 @@ func (list *LinkedList) Delete(itemIndex int) {
 // Will remove the first instance of a specified piece of data in the list
 // ARGS:
 //		data (interface{}): The data to remove
-func (list *LinkedList) Remove(data interface{}) error {
+func (list *LinkedList) Remove(data interface{}) error {	// It's common practice to return errors with certain funcs
 	for currentNode := list.head; currentNode != nil; currentNode = currentNode.next {
 		if currentNode.next.data == data {
 			currentNode.next = currentNode.next.next
@@ -69,9 +69,9 @@ func (list *LinkedList) Remove(data interface{}) error {
 // Will return the data stored at the node found at a given index
 // ARGS:
 //		itemIndex (int): The index of the data to retrieve
-func (list *LinkedList) Index(itemIndex int) interface{} {
+func (list *LinkedList) Index(itemIndex int) interface{} {	// Interfaces are super complex but can be used for basic templating
 	if itemIndex < 0 || itemIndex >= list.Size {
-		panic("Index out of range for LinkedList")
+		panic("Index out of range for LinkedList")	// Go's equivalent of an assertion (but can be "resolved" if code allows)
 	}
 
 	currentNode := list.head
@@ -83,7 +83,7 @@ func (list *LinkedList) Index(itemIndex int) interface{} {
 
 // Will display the information in the list in the terminal in a simple format
 func (list *LinkedList) Display() {
-	defer fmt.Println()
+	defer fmt.Println()	// This is a defer keyword. It will make function calls move to after the function returns (follows FILO)
 	for currentNode := list.head; currentNode != nil; currentNode = currentNode.next {
 		fmt.Printf("%v --> ", currentNode.data)
 	}
